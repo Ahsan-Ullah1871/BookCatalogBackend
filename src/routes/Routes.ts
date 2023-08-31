76c13b2e8c717f7fed1b1822b4c21f12b2f8dcaa
Ahsan-Ullah1871 <ahsanullahsunsbd@gmail.com>
2023-09-01 01:09:37 +0600

    - first commit

---
+++ src/routes/Routes.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+import { UserRoute } from '../app/modules/user/user.routes'
+import { AuthRoute } from '../app/modules/auth/auth.route'
+import { BookRoute } from '../app/modules/books/book.routes'
+import { ReviewRoute } from '../app/modules/review/review.route'
+import { WishRoute } from '../app/modules/wish/wish.route'
+import { ReadingRoute } from '../app/modules/reading/reading.route'
+
+const router = express.Router()
+
+const all_routes = [
+  { path: '/auth', router: AuthRoute },
+  { path: '/users', router: UserRoute },
+  { path: '/books', router: BookRoute },
+  { path: '/reviews', router: ReviewRoute },
+  { path: '/wish', router: WishRoute },
+  { path: '/reading', router: ReadingRoute },
+]
+
+all_routes.map(item => router.use(item.path, item.router))
+
+export default router
