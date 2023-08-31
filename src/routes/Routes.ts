2e087b7cd8fa5bd88a1c3f724aa29eb8f655e254
Ahsan-Ullah1871 <ahsanullahsunsbd@gmail.com>
2023-09-01 04:12:26 +0600

    - category works finished

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -1,22 +1,25 @@
-import express from 'express'
-import { UserRoute } from '../app/modules/user/user.routes'
-import { AuthRoute } from '../app/modules/auth/auth.route'
-import { BookRoute } from '../app/modules/books/book.routes'
-import { ReviewRoute } from '../app/modules/review/review.route'
-import { WishRoute } from '../app/modules/wish/wish.route'
-import { ReadingRoute } from '../app/modules/reading/reading.route'
+import express from "express";
+import { UserRoute } from "../app/modules/user/user.routes";
+import { AuthRoute } from "../app/modules/auth/auth.route";
+import { CategoriesRoute } from "../app/modules/category/Category.routes";
+// import { BookRoute } from "../app/modules/books/book.routes";
+// import { ReviewRoute } from "../app/modules/review/review.route";
+// import { WishRoute } from "../app/modules/wish/wish.route";
+// import { ReadingRoute } from "../app/modules/reading/reading.route";
 
-const router = express.Router()
+const router = express.Router();
 
 const all_routes = [
-  { path: '/auth', router: AuthRoute },
-  { path: '/users', router: UserRoute },
-  { path: '/books', router: BookRoute },
-  { path: '/reviews', router: ReviewRoute },
-  { path: '/wish', router: WishRoute },
-  { path: '/reading', router: ReadingRoute },
-]
+	{ path: "/auth", router: AuthRoute },
+	{ path: "/users", router: UserRoute },
+	{ path: "/categories", router: CategoriesRoute },
+	// { path: "/books", router: BookRoute },
+	// { path: "/reviews", router: ReviewRoute },
+	// { path: "/wish", router: WishRoute },
+	// { path: "/reading", router: ReadingRoute },
+];
 
-all_routes.map(item => router.use(item.path, item.router))
+all_routes.map((item) => router.use(item.path, item.router));
 
-export default router
+export default router;
+
