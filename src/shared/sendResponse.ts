5bc6dd08913368d420901f5f307906dafe281ecd
Ahsan-Ullah1871 <ahsanullahsunsbd@gmail.com>
2023-09-18 00:10:49 +0600

    - auth token updated

--- src/shared/sendResponse.ts
+++ src/shared/sendResponse.ts
@@ -1,20 +1,31 @@
-import { Response } from 'express'
+import { Response } from "express";
 
 type ISendResponse<T, K> = {
-  status_code: number
-  message: string
-  success: boolean
-  meta?: K | null
-  data: T | null
-}
+	status_code: number;
+	message: string;
+	success: boolean;
+	meta?: K | null;
+	data?: T | null;
+	token?: string;
+};
 
 const sendResponse = <T, K>(res: Response, data: ISendResponse<T, K>): void => {
-  res.status(data.status_code).json({
-    success: data.success,
-    message: data.message ?? '',
-    meta: data.meta || undefined,
-    data: data.data ?? null,
-  })
-}
+	let response_obj = data?.token
+		? {
+				success: data.success,
+				message: data.message ?? "",
+				meta: data.meta || undefined,
+				token: data.token || undefined,
+		  }
+		: {
+				success: data.success,
+				message: data.message ?? "",
+				meta: data.meta || undefined,
+				data: data.data ?? null,
+		  };
 
-export default sendResponse
+	res.status(data.status_code).json(response_obj);
+};
+
+export default sendResponse;
+
